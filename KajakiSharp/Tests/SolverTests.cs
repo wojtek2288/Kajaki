463bd250c1eda4618fe0bc4726362ac3ab5e7111
Wojciech <wojtek2288@gmail.com>
2023-05-27 14:02:13 +0200

    Add test

--- KajakiSharp/Tests/SolverTests.cs
+++ KajakiSharp/Tests/SolverTests.cs
@@ -14,7 +14,10 @@ public class SolverTests
     [InlineData(3, 5)]
     [InlineData(50, 120)]
     [InlineData(20, 190)]
-    //[InlineData(1000, 10000)]
+    [InlineData(100, 200)]
+    [InlineData(200, 400)]
+    [InlineData(500, 1000)]
+    [InlineData(1000, 2000)]
     public void Solver_works_correctly(int peopleCount, int? pairCount)
     {
         var graph = Generator.Generator.GenerateGraph(peopleCount, pairCount);
@@ -31,12 +34,34 @@ public class SolverTests
         Assert.Equal(graph.EdgeDictionary.Count, convertedRes.Count);
     }
 
+    [Fact]
+    public void Solver_correctly_solves_example_from_documentation()
+    {
+        var graph = new Graph(4);
+
+        graph.AddEdge(0, 1);
+        graph.AddEdge(1, 2);
+        graph.AddEdge(2, 3);
+        graph.AddEdge(1, 3);
+
+        var actual = Solver.Solve(graph);
+
+        var expected = new List<((int, int), (int, int)?)>
+        {
+            ((0, 1), (2, 3)),
+            ((1, 2), null),
+            ((1, 3), null),
+        };
+
+        Assert.Equal(expected, actual);
+    }
+
     private static List<(int, int)> ConvertAndSkipNulls(List<((int, int) pair1, (int, int)? pair2)> solution)
     {
         List<(int, int)> convertedList = solution
             .SelectMany(p => new[] { p.pair1, p.pair2 })
             .Where(p => p.HasValue)
-            .Select(p => p.Value)
+            .Select(p => p!.Value)
             .ToList();
 
         return convertedList;
@@ -66,6 +91,13 @@ public class SolverTests
         graph.AddEdge(8, 9);
 
         var res = graph.FindMaxMatching();
+        var usedVertices = new HashSet<int>();
+
+        foreach (var edge in res)
+        {
+            Assert.True(usedVertices.Add(edge.vertice1));
+            Assert.True(usedVertices.Add(edge.vertice2));
+        }
 
         var maxMatching = new List<(int vertice1, int vertice2)>()
         {
